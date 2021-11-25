b49173da955ad1a856601080fe5f0312ee3890d7
Maciej Ragus <maciej.ragus@schange.com>
2021-11-25 19:45:40 +0100

    Added instruction class and json file with colors

---
+++ GameBoyBoy/GBLib/Architecture/Implementations/CPU/Instruction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBLib.Architecture.Implementations.CPU
+{
+    internal class Instruction
+    {
+        public int MyProperty { get; set; }
+    }
+}
